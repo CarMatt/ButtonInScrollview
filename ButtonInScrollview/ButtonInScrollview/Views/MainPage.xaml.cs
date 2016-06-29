3a0e364d96be014fca696d16f6d8587cc05740e2
CarMatt <mc@hogia.se>
2016-06-29 22:52:54 +0200

    Add project files.

---
+++ ButtonInScrollview/ButtonInScrollview/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ButtonInScrollview.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
